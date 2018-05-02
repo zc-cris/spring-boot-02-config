4784f48e7d01f0f47851846eae864390200a57e5
zc-cris <17623887386@163.com>
2018-05-02 22:17:22 +0800

    SpringBoot 默认的日志框架为SLF4J 日志门面和 logback 日志实现包 1. 可以根据需要将不同框架的日志组件统一为logback 日志实现包（推荐）或者其他日志实现包（log4j，log4j2，logging），SLF4j 官方为我们提供了不同日志实现的适配器，方便日志框架统一管理 2. 建议使用spring 后缀的日志配置文件，将配置文件交给SpringBoot 框架来解析，可以使用更加高级的功能（比如根据开发环境的不同选择不同的日志配置）

--- src/test/java/com/zc/cris/springboot02config/SpringBoot02ConfigApplicationTests.java
+++ src/test/java/com/zc/cris/springboot02config/SpringBoot02ConfigApplicationTests.java
@@ -3,6 +3,8 @@ package com.zc.cris.springboot02config;
 import com.zc.cris.springboot02config.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -22,8 +24,13 @@ public class SpringBoot02ConfigApplicationTests {
     @Autowired
     ApplicationContext ioc;
 
+    // slf4j 的日志记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+
+
     @Test
-    public void test(){
+    public void test() {
         boolean b = ioc.containsBean("helloService");
         System.out.println(b);  // true
     }
@@ -31,6 +38,20 @@ public class SpringBoot02ConfigApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(person);
+    }
+
+    @Test
+    public void testSLF4j() {
+
+        // 日志级别由低到高：trace<debug<info<warn<error
+        logger.trace("这是trace级别的日志");
+        logger.debug("这是debug级别的日志");
+        // springBoot 默认打印info及以上级别的日志信息：默认的root级别规则
+        // 可以在配置文件中自定义设置日志级别
+        logger.info("这是info级别的日志");
+        logger.warn("这是warn级别的日志");
+        logger.error("这是error级别的日志");
+
     }
 
 }
